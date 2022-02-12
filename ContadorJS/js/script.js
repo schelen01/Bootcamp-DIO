fb99c2c272716141bab8e720ed49b0033fa580b4
schelen01 <schelengrossel@gmail.com>
2022-02-12 18:59:39 -0300

    Add exercício Contador e Perfil

---
+++ ContadorJS/js/script.js
@@ -0,0 +1,20 @@
+
+/* o span é o currentNumberWrapper*/
+var currentNumberWrapper = document.getElementById('currentNumber');
+/* é a nova variável que inicia a opreação*/
+var currentNumber = 0;
+
+function increment () {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber; /* innerHTML é o HTML que está dentro do currentNumberWrapper*/
+
+}
+
+function decrement () {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function test () {
+    kdowkdpo;
+}
